2dd3a935f54bba7532d16790a101244c76a3b9f7
Natalie <nat.vincent@gmail.com>
2017-02-03 14:16:38 +1100

    You can now use nested path elements in the unit path.

--- Nant.Contrib.Tasks.Delphi/UnitPath.cs
+++ Nant.Contrib.Tasks.Delphi/UnitPath.cs
@@ -10,36 +10,10 @@ using System.IO;
 
 namespace Nant.Contrib.Tasks.Delphi
 {
-    [Serializable]
-    public class PathElement : Element
-    {
-        private string _name;
-
-        #region Public Instance Properties
-
-        [TaskAttribute("name", Required = true)]
-        public string DirName
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-
-        #endregion
-
-        #region Protected Instance Properties
-
-        #endregion Protected Instance Properties
-
-        #region Explicit Interface Implementation
-
-        #endregion
-    }
-
 
     [ElementName("unitpath")]
     public class UnitPath : PathSet
     {
-        private PathElement[] _directoryNames;
         private bool _includeDelphiLib = false;
         private bool _usePathsAsIs = false;
 
@@ -68,13 +42,6 @@ namespace Nant.Contrib.Tasks.Delphi
             {
                 _usePathsAsIs = value;
             }
-        }
-
-        [BuildElementArray("path")]
-        public PathElement[] Paths
-        {
-            get { return _directoryNames; }
-            set { _directoryNames = value; }
         }
 
         public virtual object Clone()
